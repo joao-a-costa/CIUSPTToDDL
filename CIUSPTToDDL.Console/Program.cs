bcba481bf04b9d8840bdc7321b1849f1a6dde59e
João Costa <jcosta@smartdigit.pt>
2024-04-08 16:47:38 +0100

    Initial commit;

---
+++ CIUSPTToDDL.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+/// <summary>
+/// Parses the given XML file containing a CIUSPT invoice and maps it to an ItemTransaction object.
+/// </summary>
+/// <param name="fileToParse">The XML file content to parse.</param>
+/// <returns>An ItemTransaction object representing the parsed invoice.</returns>
+var itemTransaction = CIUSPTToDDL.CIUSPTToDDL.Parse(CIUSPTToDDL.Console.Properties.Resources.ciusptSampleFile);
+
+/// <summary>
+/// JSON serialization options.
+/// </summary>
+var serializeOptions = new JsonSerializerSettings
+{
+    NullValueHandling = NullValueHandling.Ignore,
+};
+
+/// <summary>
+/// Serialize the item transaction object to JSON format.
+/// </summary>
+var serializeJson = JsonConvert.SerializeObject(itemTransaction, Formatting.Indented, serializeOptions);
+
+Console.WriteLine(serializeJson);
